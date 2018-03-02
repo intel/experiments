7bd45289dca3f910c80b69873fae81d87083d705
Connor Doyle <connor.p.d@gmail.com>
2018-03-02 15:39:12 -0800

    Annotated tests for clarity.

--- tests/libexp_test.py
+++ tests/libexp_test.py
@@ -43,12 +43,23 @@ def test_ux_flow():
     assert exp.name == 'test'
 
     # Create a run for the test experiment
+    #
+    #   This happens in the parameter search implementation.
+    #   This project can provide some default algorithms, or the experiment
+    #   author could emit parameter space samples however they like.
+    #
+    #   This project will provide a controller to materialize experiment
+    #   runs into parameterized jobs.
     params = {'x': 3.14, 'y': 5.0, 'z': False}
     run = c.create_run(libexp.Run('test-1', params, exp.name, exp.uid()))
 
     assert run.name == 'test-1'
 
     # Publish results for the test run
+    #
+    #   This happens from the job container itself.
+    #   The controller will pass sufficient info via environment variables
+    #   so that it's easy to fetch the current run and update it.
     run.record_results({'fitness': 0.86})
     run = c.update_run(run)
 
